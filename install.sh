c9ec3d888bbb81a8bda79e7daff003aa486ac4b9
shved <shvedkuzmin2@gmail.com>
2023-10-29 05:24:17 +0100

    Added VencordDesktop folder

--- install.sh
+++ install.sh
@@ -10,6 +10,9 @@ elif [[ -d $HOME/.config/BetterDiscord ]]; then
 	bd_folder="$HOME/.config/BetterDiscord"
 	curl -s "$target_file" > "$bd_folder"/themes/gruvbox.theme.css
 	echo "Done"
+elif [[ -d $HOME/.config/VencordDesktop ]]; then 
+	vencord_desktop_folder="$HOME/.config/VencordDesktop/VencordDesktop"
+	curl -s "$target_file" > "$vencord_desktop_folder"/themes/gruvbox.theme.css
 else
 	echo "BetterDiscord or Vencord are not installed on your system (cannot find config dirs)"
 fi
